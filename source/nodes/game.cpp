f6f3a6c135a18038d6c785d849e7f7c52f162acd
Annie <anniezhang848@gmail.com>
2024-10-27 21:50:53 -0400

    Added Useful Buttons
    Added buttons for Interact [E] Journal [J] and Inventory [I] - they just work as alternate pause buttons for now

--- source/nodes/game.cpp
+++ source/nodes/game.cpp
@@ -35,17 +35,30 @@ void Game::_process(double delta) {
     Input* _input = Input::get_singleton();
     // Pause toggle with ESC key
     if (_input->is_action_just_pressed("pause_game")) {
-
         main_scene->toggle_pause();
     }
-
+    
+    // E to interact
+    // J to open journal
+    // I to open inventory (may or may not need both)
+    if (_input->is_action_just_pressed("interact")) {
+        if(DEBUG) UtilityFunctions::print("ACTION");
+    }
+    if (_input->is_action_just_pressed("journal")) {
+        main_scene->toggle_pause();
+        // Temporarily using pause, should eventually be its own check
+        // ie. If game is not paused and journal is closed, pause and open journal
+        // If game is paused and journal is closed, open journal
+        // If game is paused and journal is open, unpause and close journal
+        // Same for inventory (or whichever one we end up keeping)
+        if(DEBUG) UtilityFunctions::print("OPEN JOURNAL");
+    }
+    if (_input->is_action_just_pressed("inventory")) {
+        main_scene->toggle_pause();
+        if(DEBUG) UtilityFunctions::print("OPEN INVENTORY");
+    }
 
 }
-
-
-
-
-
 
 
 void Game::_pause() {
