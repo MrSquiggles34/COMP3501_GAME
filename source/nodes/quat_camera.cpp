bcc43c1e66cfb1d5e09de74040d76eb11e7a3a5d
MrSquiggles <57115950+MrSquiggles34@users.noreply.github.com>
2024-12-07 20:00:14 -0500

    Update quat_camera.cpp

--- source/nodes/quat_camera.cpp
+++ source/nodes/quat_camera.cpp
@@ -115,7 +115,7 @@ void QuatCamera::toggle_pause(bool paused) {
 		screen_space_shader_material->set_shader(shader);
 	}
 	else {
-		Ref<Shader> shader = ResourceLoader::get_singleton()->load("shaders/no-effect.gdshader", "Shader");
+		Ref<Shader> shader = ResourceLoader::get_singleton()->load("shaders/simplet.gdshader", "Shader");
 		screen_space_shader_material->set_shader(shader);
 	}
 
